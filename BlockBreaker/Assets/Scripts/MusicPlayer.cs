d4a14ea66bcb164a03d40baa4569277af19c0521
Bhanu <uabhanu@gmail.com>
2017-08-03 22:27:13 +0100

    Section 5 Lecture 66 Finished

---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    static MusicPlayer m_instance;
+
+	void Start()
+    {
+        if(m_instance != null) //Self Destruct if an instance already exists
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            m_instance = this; //This is the global instance as in, the Class level instance itself, and don't destroy this
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
