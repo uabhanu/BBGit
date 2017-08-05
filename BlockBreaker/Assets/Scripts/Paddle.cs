2341847c474b15c14455ce0edf1110a78535efc3
Bhanu <uabhanu@gmail.com>
2017-08-05 13:17:27 +0100

    Section 5 Lecture 76 Finished

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+        float mousePosition = (Input.mousePosition.x / Screen.width) * 16;
+        Vector3 paddlePosition = new Vector3(0.5f , transform.position.y , 0f);
+        paddlePosition.x =Mathf.Clamp(mousePosition , 0.5f , 15.5f);
+        transform.position = paddlePosition;
+	}
+}
