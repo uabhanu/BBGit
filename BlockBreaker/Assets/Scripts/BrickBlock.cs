f1234a84bda3235ecc86f45fcde764f384210c65
Bhanu <uabhanu@gmail.com>
2017-08-06 15:30:08 +0100

    Section 5 Lecture 86 WIP

--- BlockBreaker/Assets/Scripts/BrickBlock.cs
+++ BlockBreaker/Assets/Scripts/BrickBlock.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class BrickBlock : MonoBehaviour
 {
-    [SerializeField] int m_hitsLeft;
+    [SerializeField] int m_hitsTaken , m_maxHits;
+    [SerializeField] Sprite[] m_brickSprites;
+    SpriteRenderer m_brickRenderer;
 
 	void Start()
     {
-		
+		m_brickRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	void Update()
     {
-		if(m_hitsLeft == 0)
+		if(m_hitsTaken >= m_maxHits)
         {
             this.gameObject.SetActive(false);
         }
+        else
+        {
+            LoadSprite();
+        }
 	}
+
+    void LoadSprite()
+    {
+        int spriteIndex = m_hitsTaken - 1;
+        m_brickRenderer.sprite = m_brickSprites[spriteIndex];
+    }
 
     void OnCollisionEnter2D(Collision2D col2D)
     {
         if(col2D.gameObject.tag.Equals("Ball"))
         {
-            m_hitsLeft--;
+            m_hitsTaken++;
         }
     }
 }
