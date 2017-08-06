1bedfebfdae274197a6b601fe03d389629622fc4
Bhanu <uabhanu@gmail.com>
2017-08-06 09:59:21 +0100

    Bricks Game Level01 Perfectly finished!!! Section 5 Lecture 81 Finished

---
+++ BlockBreaker/Assets/Scripts/BrickBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBlock : MonoBehaviour
+{
+    [SerializeField] int m_hitsLeft;
+
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		if(m_hitsLeft == 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D col2D)
+    {
+        if(col2D.gameObject.tag.Equals("Ball"))
+        {
+            m_hitsLeft--;
+        }
+    }
+}
