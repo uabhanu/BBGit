93d505d685d921a78930d3694ea20aa87fa64639
Bhanu <uabhanu@gmail.com>
2017-08-06 11:33:11 +0100

    Minor bugs fixed with LevelLoader and it's prefab created Lose Scene and probably Win Scene loading to slow, fix WIP Section 5 Lecture 82 Finished

---
+++ BlockBreaker/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] GameObject[] m_levelPrefabs;
+    GameObject m_levelToLoad;
+    [SerializeField] int m_playerLevel;
+
+    void Reset()
+    {
+        m_playerLevel = 0;
+    }
+
+    void Start()
+    {
+        m_levelToLoad = GameObject.FindGameObjectWithTag("Level");
+
+        if(m_levelToLoad == null)
+        {
+            m_levelToLoad = Instantiate(m_levelPrefabs[m_playerLevel] , transform.position , Quaternion.identity); 
+        }
+    }
+}
